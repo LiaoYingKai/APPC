a91291b7e1587568ee81f000e9cc38f1613021e2
Kai <b10223052@yuntech.org.tw>
2023-05-17 23:04:48 +0800

    feat: add router

--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,30 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      redirect: {
+        name: 'collection'
+      },
+      children: [
+        {
+          path: 'collection',
+          name: 'collection',
+          component: () => import('../views/collection/index.vue'),
+          redirect: {
+            name: 'collection-list'
+          },
+          children: [
+            {
+              path: 'list',
+              name: 'collection-list',
+              component: () => import('../views/collection/List.vue')
+            }
+          ]
+        }
+      ]
     }
   ]
 })
